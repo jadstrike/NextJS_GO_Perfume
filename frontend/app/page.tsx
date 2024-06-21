695e270b9baddbd95b131151b86ec7b480f162d4
issac <63854143+jadstrike@users.noreply.github.com>
2024-06-21 20:19:16 +0700

    home page created

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { ExampleHomePage } from "@/components/example-home-page";
+
+export default function Home() {
+  return (
+    <div>
+      <ExampleHomePage />
+    </div>
+  );
+}
