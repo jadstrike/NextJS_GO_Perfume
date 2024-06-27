16fea0e03d96e3cdffc830f12fce30c36b5a38bb
issac <issaciscoding@gmail.com>
2024-06-28 02:25:23 +0700

    initial landing page"

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+"use client";
+import LandingPage from "@/components/animation/landing";
 import { ExampleHomePage } from "@/components/example-home-page";
 
 export default function Home() {
   return (
     <div>
-      <ExampleHomePage />
+      {/* <ExampleHomePage /> */}
+      <LandingPage />
     </div>
   );
 }
